7f55df5213157b3eccafb581cc10da0f5718a0bb
Libin <1064048947@qq.com>
2022-06-12 10:21:13 +0800

    自定义router

--- src/krouter/index.js
+++ src/krouter/index.js
@@ -1,8 +1,27 @@
 import Vue from "vue"
 import KVueRouter from "./kvue-router"
+import Home from '../views/Home.vue'
 
 Vue.use(KVueRouter)
 
-const router = new KVueRouter()
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  }
+]
+
+const router = new KVueRouter({
+  routes
+})
 
 export default router 
--- src/krouter/kvue-router.js
+++ src/krouter/kvue-router.js
@@ -1,17 +1,30 @@
-
 let Vue
 
 class KVueRouter {
-  constructor() {
-    console.log('@@', Vue)
+  constructor(options) {
+    this.$options = options
+
+    // current 必须是响应式的？如何做到？
+    // Vue.set 是不可以的 因为他需要对象本身是一个响应式对象
+    Vue.util.defineReactive(this, 'current', window.location.hash.slice(1) || '/')
+    // this.current = window.location.hash.slice(1) || '/'
+    window.addEventListener('hashchange', () => {
+      this.current =  window.location.hash.slice(1)
+    })
   }
 }
 
 KVueRouter.install = (_V) => {
   Vue = _V
+  Vue.mixin({
+    beforeCreate() {
+      if (this.$options.router) {
+        Vue.prototype.$router = this.$options.router
+      }
+    }  
+  })
   // 声明两个全局组件: router-link/router-view
   Vue.component('router-link', {
-    // template: '<a>router-link</a>'
     props: {
       to: {
         type: String,
@@ -30,9 +43,17 @@ KVueRouter.install = (_V) => {
   })
 
   Vue.component('router-view', {
-    // template: '<div>router-view</div>'
+    // render 什么时候会执行
+    // init 执行 && 响应式数据变化再次执行
     render(h) {
-      return h('div', 'router-view')
+      let component = null
+      const route = this.$router.$options.routes.find(
+        route => route.path === this.$router.current
+      )
+      if (route) {
+        component = route.component
+      }
+      return h(component)
     }
   })
 }
